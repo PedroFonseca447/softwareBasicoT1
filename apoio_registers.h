143a2fea2087c998c54de66ba466dc6098b42e57
Pedro B B Fonseca <84049639+PedroFonseca447@users.noreply.github.com>
2024-04-26 14:41:48 -0300

    Update apoio_registers.h

--- apoio_registers.h
+++ apoio_registers.h
@@ -4,22 +4,7 @@
 #include <stdint.h>
 
 // Definições dos registradores
-extern uint16_t *R0;
-extern uint16_t *R1;
-extern uint16_t *R2;
-extern uint16_t *R3;
-extern uint16_t *R4;
-extern uint16_t *R5;
-extern uint16_t *R6;
-extern uint16_t *R7;
-extern uint16_t *R8;
-extern uint16_t *R9;
-extern uint16_t *R10;
-extern uint16_t *R11;
-extern uint16_t *R12;
-extern uint16_t *R13;
-extern uint16_t *R14;
-extern uint16_t *R15;
+
 
 // Funções para leitura e escrita nos registradores
 uint16_t read_register(uint16_t *reg);
