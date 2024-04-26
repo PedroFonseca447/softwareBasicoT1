296d4b32ff68ffbf6d5288030f75255ba755303e
pedro_fonseca <pedrofonseca447@gmail.com>
2024-04-26 14:19:17 -0300

    arrumando o lib

--- apoio_registers.h
+++ apoio_registers.h
@@ -1,34 +1,34 @@
-// register_utils.h
+#ifndef REGISTERS_H
+#define REGISTERS_H
 
-#ifndef REGISTER_UTILS_H
-#define REGISTER_UTILS_H
+#include <stdint.h>
 
-#include <stdio.h>
+// Definições dos registradores
+extern uint16_t *R0;
+extern uint16_t *R1;
+extern uint16_t *R2;
+extern uint16_t *R3;
+extern uint16_t *R4;
+extern uint16_t *R5;
+extern uint16_t *R6;
+extern uint16_t *R7;
+extern uint16_t *R8;
+extern uint16_t *R9;
+extern uint16_t *R10;
+extern uint16_t *R11;
+extern uint16_t *R12;
+extern uint16_t *R13;
+extern uint16_t *R14;
+extern uint16_t *R15;
 
-// Define o tipo de dado para representar um registrador de 16 bits
-typedef unsigned short Register;
+// Funções para leitura e escrita nos registradores
+uint16_t read_register(uint16_t *reg);
+void write_register(uint16_t *reg, uint16_t value);
 
-// Define um tipo de enumeração para representar as cores
-typedef enum {
-    NO_COLOR,
-    RED,
-    GREEN,
-    BLUE,
-} Color; 
+// Funções de controle para o hardware
+void display_message(const char *message);
+void control_status_led(int battery_level);
 
-typedef enum {
-    CRITICO,
-    BAIXO,
-    MEDIO,
-    ALTO,
-
-} Batery;
-
-// Função para determinar a cor com base nos bits 10,11 e 12 do registrador
+#endif /* REGISTERS_H */
 
 
-Color read_color(Register *r1,Register  *r2);
-
-Batery read_bateryLevel(Register *r3);
-
-#endif
