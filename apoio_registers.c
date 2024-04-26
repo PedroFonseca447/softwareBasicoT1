f5f836edf87080b906a835b218a27382f11488ea
Pedro B B Fonseca <84049639+PedroFonseca447@users.noreply.github.com>
2024-04-26 01:40:57 -0300

    Update apoio_registers.c

--- apoio_registers.c
+++ apoio_registers.c
@@ -1,100 +1,119 @@
-// register_utils.c
-#include <stdint.h>
-#include "apoio_registers.h"
+#include "registers.h"
 
-// Função para determinar a cor com base nos bits 8 e 9 do registrador
+// Implementação dos registradores
+uint16_t R0_data;
+uint16_t R1_data;
+uint16_t R2_data;
+uint16_t R3_data;
+uint16_t R4_data;
+uint16_t R5_data;
+uint16_t R6_data;
+uint16_t R7_data;
+uint16_t R8_data;
+uint16_t R9_data;
+uint16_t R10_data;
+uint16_t R11_data;
+uint16_t R12_data;
+uint16_t R13_data;
+uint16_t R14_data;
+uint16_t R15_data;
 
-
-
-    void set_color(Register *r0, Register *r1, Register *r2, int bit_10, int bit_11, int bit_12){
-
-        if( bit_10 == 1){
-            *r0 |= (0x1 << 10); //ativa o bit de cor RED
-           
-                *r1 |= 255;//do 0-7 preenche com 255
-            
-        } else if( bit_10 == 0){
-          *r0 &= ~(0x1 << 10);//zera o bit 10
-          *r1 &= ~(0xFF);//zera os bit de color em r1 do intervalo de 0-7
-        }
-
-        if( bit_11 == 1){
-            *r0 |= (0x1 << 11);//seta para 1 o bit 11 no registrador 0 
-
-               *r1 |= (255<<8);// preenche os bits de cor em verde 
-              
-        } else if( bit_11== 0){
-         *r0 &= ~(0x1 << 10);//zera o bit 10
-          *r1 &= ~(0xFF00);//zera os bit de color em r1 do intervalo 8-15
-        }
-        
-        if( bit_12 == 1){
-         
-         *r0 |= (0x1 << 12); // ativa o bit de cor BLUE
-
-          *r2 |= 255;// preenche os bits de cor em verde no registrador
-
-        }   else if( bit_12 == 0){
-         *r0 &= ~(0x1 << 12);//zera o bit 12
-          *r1 &= ~(0xFF);//zera os bit de color em r1 do intervalo 0-7
-        }
-
-
-        
+// Função para ler do registrador
+uint16_t read_register(uint16_t *reg) {
+    switch ((uintptr_t)reg) {
+        case (uintptr_t)&R0:
+            return R0_data;
+        case (uintptr_t)&R1:
+            return R1_data;
+        case (uintptr_t)&R2:
+            return R2_data;
+        case (uintptr_t)&R3:
+            return R3_data;
+        case (uintptr_t)&R4:
+            return R4_data;
+        case (uintptr_t)&R5:
+            return R5_data;
+        case (uintptr_t)&R6:
+            return R6_data;
+        case (uintptr_t)&R7:
+            return R7_data;
+        case (uintptr_t)&R8:
+            return R8_data;
+        case (uintptr_t)&R9:
+            return R9_data;
+        case (uintptr_t)&R10:
+            return R10_data;
+        case (uintptr_t)&R11:
+            return R11_data;
+        case (uintptr_t)&R12:
+            return R12_data;
+        case (uintptr_t)&R13:
+            return R13_data;
+        case (uintptr_t)&R14:
+            return R14_data;
+        case (uintptr_t)&R15:
+            return R15_data;
+        default:
+            return 0; // Registro não encontrado
+    }
 }
 
-    Color read_color(Register *r1, Register *r2){
-
-        uint8_t red_component = *r1 & 0xFF;
-        uint8_t green_component = (*r1 >> 8) & 0xFF;
-        uint8_t blue_component = *r2 & 0xFF;
-
-
-     if(red_component == 255 && green_component == 0 && blue_component == 0){
-        return RED;
-     }else if(red_component == 0 && green_component == 255 && blue_component == 0){
-        return GREEN;
-     } else if(red_component == 0 && green_component == 0 && blue_component == 255){
-        return BLUE;
-     } else {
-        return NO_COLOR;
-     }
-
+// Função para escrever no registrador
+void write_register(uint16_t *reg, uint16_t value) {
+    switch ((uintptr_t)reg) {
+        case (uintptr_t)&R0:
+            R0_data = value;
+            break;
+        case (uintptr_t)&R1:
+            R1_data = value;
+            break;
+        case (uintptr_t)&R2:
+            R2_data = value;
+            break;
+        case (uintptr_t)&R3:
+            R3_data = value;
+            break;
+        case (uintptr_t)&R4:
+            R4_data = value;
+            break;
+        case (uintptr_t)&R5:
+            R5_data = value;
+            break;
+        case (uintptr_t)&R6:
+            R6_data = value;
+            break;
+        case (uintptr_t)&R7:
+            R7_data = value;
+            break;
+        case (uintptr_t)&R8:
+            R8_data = value;
+            break;
+        case (uintptr_t)&R9:
+            R9_data = value;
+            break;
+        case (uintptr_t)&R10:
+            R10_data = value;
+            break;
+        case (uintptr_t)&R11:
+            R11_data = value;
+            break;
+        case (uintptr_t)&R12:
+            R12_data = value;
+            break;
+        case (uintptr_t)&R13:
+            R13_data = value;
+            break;
+        case (uintptr_t)&R14:
+            R14_data = value;
+            break;
+        case (uintptr_t)&R15:
+            R15_data = value;
+            break;
+        default:
+            break; // Registro não encontrado
+    }
 }
-// reset de todos os bits analisar mehlro em casa
-   void set_fabric(Register *r0,Register *r1, Register *r2, Register *r3, Register *r4, Register *r5,
-   Register *r6, Register *r7, Register *r8, Register *r9, Register *r10, Register *r11, Register *r12,
-   Register *r13, Register *r14, Register *r15)
-   {
 
-
-
-
-
-   }
-
-
-   Batery read_bateryLevel(Register *r3){
-
-      int bit0 = (*r3) & 0x01;
-      int bit1 = (*r3 >> 1) & 0x01;
-
-      if(bit0 == 0 && bit1 == 0){
-         return CRITICO;
-      }
-
-      if(bit0 == 0 && bit1 == 1){
-         return BAIXO;
-      }
-
-      if(bit0 == 1 && bit1 == 0){
-         return MEDIO;
-      }
-
-      if(bit0 == 1 && bit1 == 1){
-         return ALTO;
-      }
-   }
 
 
 
